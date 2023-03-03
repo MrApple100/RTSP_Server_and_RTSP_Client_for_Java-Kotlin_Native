47a4e03344624abe031fd19ee7e447b7e5b3a688
Alexander <mrapple100@yandex.ru>
2023-03-03 23:10:27 +0300

    MediaCodec constant

---
+++ src/mrapple100/utils/MediaCodec.java
@@ -0,0 +1,8 @@
+package mrapple100.utils;
+
+public final class MediaCodec {
+    public static final int BUFFER_FLAG_CODEC_CONFIG = 2;
+    public static final int BUFFER_FLAG_END_OF_STREAM = 4;
+    public static final int BUFFER_FLAG_KEY_FRAME = 1;
+    public static final int BUFFER_FLAG_PARTIAL_FRAME = 8;
+}
