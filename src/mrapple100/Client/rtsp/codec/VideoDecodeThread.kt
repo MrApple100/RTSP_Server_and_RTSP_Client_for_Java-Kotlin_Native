75ef88e40dc422d6cbc93159d93e1b817434fc96
Alexander <mrapple100@yandex.ru>
2023-03-07 23:20:49 +0300

    Code from byte[](rgb) to YUV420
    AND test comment function for errors in convert

--- src/mrapple100/Client/rtsp/codec/VideoDecodeThread.kt
+++ src/mrapple100/Client/rtsp/codec/VideoDecodeThread.kt
@@ -1,9 +1,7 @@
 package mrapple100.Client.rtsp.codec
 
 import mrapple100.Server.encoder.Frame
-import mrapple100.Server.encoder.utils.yuv.YUVUtil
 import mrapple100.Server.rtspserver.RtspServerCamera1
-import mrapple100.utils.DecodeUtil
 import org.bytedeco.ffmpeg.avcodec.AVCodec
 import org.bytedeco.ffmpeg.avcodec.AVCodecContext
 import org.bytedeco.ffmpeg.avcodec.AVPacket
@@ -13,9 +11,13 @@ import org.bytedeco.ffmpeg.global.avcodec
 import org.bytedeco.ffmpeg.global.avcodec.av_packet_unref
 import org.bytedeco.ffmpeg.global.avutil
 import org.bytedeco.ffmpeg.global.avutil.*
-import org.bytedeco.ffmpeg.global.swscale.*
+import org.bytedeco.ffmpeg.global.swscale.sws_getContext
+import org.bytedeco.ffmpeg.global.swscale.sws_scale
 import org.bytedeco.ffmpeg.swscale.SwsContext
-import org.bytedeco.javacpp.*
+import org.bytedeco.javacpp.BytePointer
+import org.bytedeco.javacpp.DoublePointer
+import org.bytedeco.javacpp.Pointer
+import org.bytedeco.javacpp.PointerPointer
 import java.awt.Image
 import java.awt.Toolkit
 import java.awt.Transparency
@@ -39,6 +41,11 @@ class VideoDecodeThread (
     private val context: AVCodecContext = avcodec.avcodec_alloc_context3(codec);
     private val opts = AVDictionary()
 
+    val frame: AVFrame = avutil.av_frame_alloc();
+    val packet: AVPacket = AVPacket()
+    var swsContext: SwsContext? = null
+
+    val rgbFrame: AVFrame = avutil.av_frame_alloc()
 
 
 
@@ -77,10 +84,10 @@ class VideoDecodeThread (
                     } else {
                         if(frameq!!.data[4]== "103".toByte()){
                             spspps = frameq!!.data
-                            println("103 "+DecodeUtil.byteArrayToHexString(spspps).subSequence(0,50))
+                            //println("103 "+DecodeUtil.byteArrayToHexString(spspps).subSequence(0,50))
                         }else{
                             val datasrc = frameq!!.data
-                            println("OST "+DecodeUtil.byteArrayToHexString(frameq!!.data).subSequence(0,50))
+                            //println("OST "+DecodeUtil.byteArrayToHexString(frameq!!.data).subSequence(0,50))
 
                          //   println(DecodeUtil.byteArrayToHexString(datasrc))
 
@@ -94,11 +101,7 @@ class VideoDecodeThread (
                             try {
                                 //////////////////////////////////////////////////////////////////////////
 
-                                 val frame: AVFrame = avutil.av_frame_alloc();
-                                 val packet: AVPacket = AVPacket()
-                                 var swsContext: SwsContext? = null
 
-                                val rgbFrame: AVFrame = avutil.av_frame_alloc()
 
                                 avcodec.avcodec_open2(context, codec, opts)
 
@@ -184,7 +187,6 @@ class VideoDecodeThread (
                                  //   println("Success!")
 
                                  //   println(""+System.currentTimeMillis()+" "+baos!!.toByteArray().size)
-                                    framePlace.icon = ImageIcon(toolkit.createImage(baos!!.toByteArray(), 0, baos.size()).getScaledInstance(600,800, Image.SCALE_DEFAULT))
 
 
 
@@ -197,23 +199,42 @@ class VideoDecodeThread (
 
 
 // Конвертировать RGB в YUV420
-                                      //  println("start")
 
-                                    val swsContext2: SwsContext = sws_getContext(context.width(), context.height(), AV_PIX_FMT_RGB24, context.width(), context.height(), AV_PIX_FMT_YUV420P, 0, null, null,  DoublePointer())
-                                      //  println("swsContext "+swsContext.address())
-                                      //  println("out "+output.size)
+                                    val frameAfterRGB = av_frame_alloc();
+                                    frameAfterRGB.width(context.width())
+                                    frameAfterRGB.height(context.height())
+                                    frameAfterRGB.format(AV_PIX_FMT_RGB24)
+                                    val pp2: PointerPointer<*> = PointerPointer<Pointer>(frameAfterRGB)
+                                    val bp2 = BytePointer(ByteBuffer.wrap(output))
+                                    av_frame_get_buffer(frameAfterRGB, 32) //было 32
+
+                                    av_image_fill_arrays(
+                                            frameAfterRGB.data(),
+                                            frameAfterRGB.linesize(),
+                                            bp2
+                                            ,
+                                            AV_PIX_FMT_RGB24,
+                                            context.width(),
+                                            context.height(),
+                                            1)
+
+                                      //  println("start")
+                                    //    println("IMG "+img!!.width+" "+img!!.height) 640x480
+                                    val swsContext2: SwsContext = sws_getContext(context.width(), context.height(), AV_PIX_FMT_RGB24 , context.width(), context.height(), AV_PIX_FMT_YUV420P, 0, null, null,  DoublePointer())
+                                    //  println("swsContext "+swsContext.address())
+                                    //  println("out "+output.size)
                                     val yuvFrame: AVFrame = avutil.av_frame_alloc()
                                     val sizeyuv = avutil.av_image_get_buffer_size(avutil.AV_PIX_FMT_YUV420P, context.width(), context.height(), 1)
                                     val bufferyuv: BytePointer = BytePointer(avutil.av_malloc(sizeyuv.toLong()))
                                     avutil.av_image_fill_arrays(yuvFrame.data(), yuvFrame.linesize(), bufferyuv, avutil.AV_PIX_FMT_YUV420P, context.width(), context.height(), 1)
 
 
-                                   // val yuv420 = ByteArray(output.size/2)
+                                    // val yuv420 = ByteArray(output.size/2)
                                     //val ppOutput = PointerPointer<Pointer>(output)
                                     //val ppYuv420 = PointerPointer<Pointer>(yuv420)
-                                    sws_scale(swsContext2,rgbFrame.data(),rgbFrame.linesize(),0,context.height(),yuvFrame.data(),yuvFrame.linesize())
-                                    val outputyuv = ByteArray(size)
-                                    buffer.get(outputyuv)
+                                    sws_scale(swsContext2,frameAfterRGB.data(),frameAfterRGB.linesize(),0,context.height(),yuvFrame.data(),yuvFrame.linesize())
+                                    val outputyuv = ByteArray(sizeyuv)
+                                    bufferyuv.get(outputyuv)
                                   //  sws_scale(swsContext2, ppOutput, IntPointer( context.width()*3), 0, context.height(), ppYuv420, IntPointer( context.width()))
                                       //  println("swsscale is ok "+ yuv420.size)
 
@@ -233,7 +254,69 @@ class VideoDecodeThread (
                                    // av_frame_free(frame)
                                    // av_frame_free(rgbFrame)
                                     // }
-                    av_packet_unref(packet)
+                                    //TESTTESTTEST
+//                                    val frame2 = av_frame_alloc();
+//                                    frame2.width(context.width())
+//                                    frame2.height(context.height())
+//                                    frame2.format(AV_PIX_FMT_YUV420P)
+//                                    val pp: PointerPointer<*> = PointerPointer<Pointer>(frame2)
+//                                    val bp = BytePointer(ByteBuffer.wrap(outputyuv))
+//                                    av_frame_get_buffer(frame2, 32) //было 32
+//
+//                                    av_image_fill_arrays(
+//                                            frame2.data(),
+//                                            frame2.linesize(),
+//                                            bp
+//                                            ,
+//                                            AV_PIX_FMT_YUV420P,
+//                                            context.width(),
+//                                            context.height(),
+//                                            1)
+////                                    val y = ByteArray(context.width() * context.height())
+////                                    val u = ByteArray(y.size / 4)
+////                                    val v = ByteArray(u.size)
+////
+////
+////                                    System.arraycopy(outputyuv, 0, y, 0, y.size)
+////                                    System.arraycopy(outputyuv, y.size, u, 0, u.size)
+////                                    System.arraycopy(outputyuv, y.size + u.size, v, 0, v.size)
+////
+////                                    frame.data(0, BytePointer(*y))
+////                                    frame.data(1, BytePointer(*u))
+////                                    frame.data(2, BytePointer(*v))
+//
+//                                    var swsContext3 = sws_getContext(
+//                                            context.width(), context.height(), context.pix_fmt(),
+//                                            context.width(), context.height(), avutil.AV_PIX_FMT_RGB24,
+//                                            0, null, null, DoublePointer())
+//
+//                                    val size2 = avutil.av_image_get_buffer_size(avutil.AV_PIX_FMT_RGB24, context.width(), context.height(), 1)
+//                                    val buffer2: BytePointer = BytePointer(avutil.av_malloc(size2.toLong()))
+//                                    avutil.av_image_fill_arrays(rgbFrame.data(), rgbFrame.linesize(), buffer2, avutil.AV_PIX_FMT_RGB24, context.width(), context.height(), 1)
+//                                    //  println("Nice3")
+//                                    sws_scale(swsContext3, frame2.data(), frame2.linesize(), 0, context.height(), rgbFrame.data(), rgbFrame.linesize())
+//                                    val output3 = ByteArray(size)
+//                                    buffer2.get(output3)
+//                                  //  println(output3.size)//h*w*3
+//
+//                                    var img3 = createRGBImage(output3, context.width(), context.height())
+//                                    //  println("img : ${(img!!.raster.dataBuffer as DataBufferByte).data[0]} ${(img!!.raster.dataBuffer as DataBufferByte).data[1]} ${(img!!.raster.dataBuffer as DataBufferByte).data[2]} ${output.size}")
+//
+//                                    var baos3: ByteArrayOutputStream? = null
+//                                    try {
+//                                        baos3 = ByteArrayOutputStream()
+//                                        ImageIO.write(img3, "jpg", baos3)
+//                                    } finally {
+//                                        try {
+//                                            baos3!!.close()
+//                                        } catch (e: java.lang.Exception) {
+//                                        }
+//                                    }
+                                    //TESTTESTTEST
+
+                                    framePlace.icon = ImageIcon(toolkit.createImage(baos!!.toByteArray(), 0, baos.size()).getScaledInstance(500,800, Image.SCALE_DEFAULT))
+
+                                    av_packet_unref(packet)
 //                    avcodec_close(context)
 //                    avcodec_free_context(context)
 //
